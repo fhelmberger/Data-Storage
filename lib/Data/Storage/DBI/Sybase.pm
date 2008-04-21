7de98387e919cf203662f88533978a1be363be74
Marcel Gruenauer <hanekomu@gmail.com>
2008-04-21 14:22:35 +0000

    lang/perl/Data-Storage: initial import
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Data-Storage/tags/Data-Storage-0.07@10093 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/Data/Storage/DBI/Sybase.pm
@@ -0,0 +1,69 @@
+package Data::Storage::DBI::Sybase;
+
+# $Id: Sybase.pm 13653 2007-10-22 09:11:20Z gr $
+
+use strict;
+use warnings;
+
+
+our $VERSION = '0.07';
+
+
+use base qw(Data::Storage::DBI Class::Accessor::Complex);
+
+
+__PACKAGE__->mk_scalar_accessors(qw(dbserver));
+
+
+sub connect_string {
+    my $self = shift;
+    sprintf("DBI:Sybase:server=%s;database=%s", $self->dbserver, $self->dbname);
+}
+
+
+# no LongReadLen
+
+sub get_connect_options {
+    my $self = shift;
+    { RaiseError  => $self->RaiseError,
+      PrintError  => $self->PrintError,
+      AutoCommit  => $self->AutoCommit,
+      HandleError => $self->HandleError,
+    }
+}
+
+
+1;
+
+
+__END__
+
+{% USE p = PodGenerated %}
+
+=head1 NAME
+
+{% p.package %} - generic abstract storage mechanism
+
+=head1 SYNOPSIS
+
+    {% p.package %}->new;
+
+=head1 DESCRIPTION
+
+None yet. This is an early release; fully functional, but undocumented. The
+next release will have more documentation.
+
+=head1 METHODS
+
+=over 4
+
+{% p.write_methods %}
+
+=back
+
+{% p.write_inheritance %}
+
+{% PROCESS standard_pod %}
+
+=cut
+
---
+++ lib/Data/Storage/Memory.pm
@@ -0,0 +1,67 @@
+package Data::Storage::Memory;
+
+# $Id: Memory.pm 9190 2005-06-14 14:47:46Z gr $
+
+use strict;
+use warnings;
+
+
+our $VERSION = '0.07';
+
+
+use base 'Data::Storage';
+
+
+sub connect {}
+
+
+sub disconnect {
+    my $self = shift;
+    return unless $self->is_connected;
+    $self->rollback_mode ? $self->rollback : $self->commit;
+}
+
+
+sub is_connected { 1 }
+
+
+# might implement this later using transactional hashes or some such
+
+sub rollback {}
+sub commit   {}
+
+
+1;
+
+
+__END__
+
+{% USE p = PodGenerated %}
+
+=head1 NAME
+
+{% p.package %} - generic abstract storage mechanism
+
+=head1 SYNOPSIS
+
+    {% p.package %}->new;
+
+=head1 DESCRIPTION
+
+None yet. This is an early release; fully functional, but undocumented. The
+next release will have more documentation.
+
+=head1 METHODS
+
+=over 4
+
+{% p.write_methods %}
+
+=back
+
+{% p.write_inheritance %}
+
+{% PROCESS standard_pod %}
+
+=cut
+
